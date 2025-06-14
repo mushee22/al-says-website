c4cdf005acc85d2d8c63002af85842bdd8f47c36
mushee22 <mmtmusheer@gmail.com>
2025-06-14 07:33:10 +0530

    fix: correct apostrophe rendering in sustainability hero section title

--- components/sections/sustainability/hero.tsx
+++ components/sections/sustainability/hero.tsx
@@ -11,7 +11,7 @@ export default function HeroSection() {
                 className="-z-10 object-cover object-left"
             />
             <div className="container flex flex-col justify-end h-full pb-6 sm:pb-12 md:pb-32  space-y-7">
-                <h1 className="title text-primary max-w-[543px]">AL Say's Commitment to Sustainability</h1>
+                <h1 className="title text-primary max-w-[543px]">AL Say{"'"}s Commitment to Sustainability</h1>
                 <p className="max-w-[543px] text-grey-800 body-md">
                     At All Says Trading Co., sustainability is integrated into every aspect of our operations.
                     We are committed to minimizing environmental impact, promoting social responsibility,
